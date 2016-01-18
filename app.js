43a10a592fb788b757520fd695f2814827d29f3d
vikashiscool <vikash_parekh@yahoo.com>
2016-01-18 15:47:43 -0800

    Entry point is app.js (server). index.js will hold all other JavaScript

---
+++ app.js
@@ -0,0 +1,5 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
--- server.js
+++
@@ -1,5 +0,0 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    app = express();
-
-app.use(bodyParser.urlencoded({extended: true}))
